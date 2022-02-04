cd0920ded672e9d0e900343ccb7fed4356419dea
jorge1019 <jorge10.lominchar@gmail.com>
2022-02-04 12:32:18 +0100

    Clase 04/02/2022

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ANGULAR';
   texto="Bonitos dias"
+  
+public mostrar_videojuegos:boolean=true;//creamos una variable publica para poder visualizar y ocultar el componente
+public mostrar_zapatillas:boolean=true;
+//creamos un metodo para poder crear y destruir el componente videojuegos
+ocultarVideojuegos(valor:boolean){
+this.mostrar_videojuegos=valor;
+
+}
+ocultarZapatillas(valor2:boolean){
+  this.mostrar_zapatillas=valor2;
+  
+  }
+
   }
