3d4169ad03dacde4ae2020b10b3a0a6cdb59e186
jorge1019 <jorge10.lominchar@gmail.com>
2022-02-10 11:42:59 +0100

    CLASE 09/02/2022

--- Angular/src/app/zapatillas/zapatillas.component.ts
+++ Angular/src/app/zapatillas/zapatillas.component.ts
@@ -8,26 +8,41 @@ import { Zapatilla } from '../models/zapatilla';
 export class ZapatillasComponent implements OnInit,OnDestroy,DoCheck {
 public titulo:string="Componente de Zapatillas";
 public zapatillas:Array<Zapatilla>; // CREAMOS EL ARRAY 
-
+public marcas:Array<string>;
 
 
 
 
   constructor() {
-  
-    this.zapatillas = [new Zapatilla ("Jordar Air ","Nike","Azul",200,false)],
-                      [new Zapatilla ("Reebok Classic ","Reebok","Verde",120,true)],
-                      [new Zapatilla ("Adidas Supreme","Adidas","Blancas",150,true)]
+    this.marcas = new Array;
+    this.zapatillas = [new Zapatilla ("Jordar Air ","Nike","Azul",200,false),
+                      new Zapatilla ("Reebok Classic ","Reebok","Verde",120,true),
+                      new Zapatilla ("Adidas Supreme","Adidas","Blancas",150,true),
+                      new Zapatilla ("Nike Air","Nike","Rojas",300,false)]
   }
 
   ngDoCheck(): void {
       
   }
   ngOnInit(): void {
-      
+   
+   
+
   }
   ngOnDestroy(): void {
       
   }
+
+getMarcas(){
+
+this.zapatillas.forEach((zapatilla,index)=>{
+
+if (this.marcas.indexOf(zapatilla.marca)<0){
+  this.marcas.push(zapatilla.marca);
+}
+
+}
+)
+}
 
 }
